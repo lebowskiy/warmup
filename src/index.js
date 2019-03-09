9fac5d2f20a4818f20a0684ff244fa9b857a2514
andrei.kishkevich <bydron@gmail.com>
2019-03-09 11:02:16 +0300

    finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  if(typeof temperature == 'number') {
+    return (temperature * (9/5)) + 32
+  } else {
+    console.log("temperature is not a 'nubmer'")
+    return convetTemp 
+  }
 };
